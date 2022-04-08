5b42bc5c39a20758629f3161df80d905f87cf0df
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-04-08 11:20:07 -0700

    commit to master

---
+++ 5. Roman Numerals Encoder/index.js
@@ -0,0 +1,39 @@
+/* 
+
+Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+
+Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+EX:
+solution(100); // should return 'M'
+
+*/
+
+const solution = (number) => {
+	let result = "",
+		decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+		roman = [
+			"M",
+			"CM",
+			"D",
+			"CD",
+			"C",
+			"XC",
+			"L",
+			"XL",
+			"X",
+			"IX",
+			"V",
+			"IV",
+			"I",
+		];
+
+	decimals.map(function (value, index) {
+		while (number >= value) {
+			result += roman[index];
+			number -= value;
+		}
+	});
+
+	return result;
+};
