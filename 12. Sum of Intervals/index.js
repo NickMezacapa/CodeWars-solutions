77c1c07cd622238e0c205b1dee59b1224907031c
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-04-14 13:42:03 -0700

    commit to master

--- 12. Sum of Intervals/index.js
+++ 12. Sum of Intervals/index.js
@@ -41,3 +41,29 @@ Random Tests
 100 tests with 20000 - 50000 intervals from the range [-10^9, 10^9]
 
 */
+
+function sumIntervals(intervals){
+    intervals = intervals.sort(function(a, b) { // O(n)
+      return a[0] - b[0];
+    });
+    intervals = intervals.reduce(function(acc, el, index, array) { // O(n)
+      const anterior = array[index - 1];
+      if (array.length > 1 && anterior !== undefined) {
+        if (el[0] < acc[acc.length - 1]) {
+          if (el[1] >= acc[acc.length - 1]) {
+            acc[acc.length - 1] = el[1];
+          }
+        } else {
+          acc.push(...el);
+        }
+      } else {
+        acc.push(...el);
+      }
+      return acc;
+    }, []);
+    let result = 0;
+    for (let i = 0; i < intervals.length - 1 ; i+=2) { // O(2n)
+      result+=(intervals[i + 1] - intervals[i]);
+    }
+    return result;
+  } // O(n) + O(n) + O(2n) = 3O(n) = O(n)
