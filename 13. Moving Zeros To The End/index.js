3bfa54c43c6243ea62547f006b20cfec759de749
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-04-16 12:17:16 -0700

    commit to master

--- 13. Moving Zeros To The End/index.js
+++ 13. Moving Zeros To The End/index.js
@@ -10,11 +10,11 @@ moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 // After the loop is finished push the zero holder array back to the original array
 
 const moveZeros = (arr) => {
-    let zeros = arr.filter(element => element === 0).length;
-    let result = arr.filter(element => element !== 0);
+    let zeros = arr.filter(element => element === 0).length; // Variable to store the amount of zeros
+    let result = arr.filter(element => element !== 0); // return everything that is not zero
 
     for (let i = 0; i < zeros; i++) {
-        result.push(0);
+        result.push(0); 
     }
     return result;
 }
