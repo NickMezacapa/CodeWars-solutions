ff99e46bfec79872dd6e558b925c4a8157371ed1
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-04-17 21:09:33 -0700

    commit to master

--- 14. Can you get the loop? -5kyu/index.js
+++ 14. Can you get the loop? -5kyu/index.js
@@ -7,3 +7,30 @@ node.getNext()
 node.next
 
 */
+
+function loop_size(node){
+    let size = 1;
+    let seenNode = getNodeInLoop(node); 
+    let pointer = seenNode.next; 
+
+    while(pointer !== seenNode ){
+        size++; 
+        pointer = pointer.next;
+    }
+
+    return size;
+}
+
+function getNodeInLoop(node){
+   let slow = node;
+   let fast = node.next;
+
+//problem assumes there is always going to be a loop
+//so no need to check
+   while(slow !== fast){ 
+        slow = slow.next; //move by 1
+        fast = fast.next.next; //move by 2
+    }
+
+  return slow; 
+}
