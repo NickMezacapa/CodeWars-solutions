3b3a50058878ffd2fc7a85168dae8af3b17577fc
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-04-20 10:50:06 -0700

    commit to master

---
+++ 20. Removing Zeros -4kyu/index.js
@@ -0,0 +1,58 @@
+/* 
+
+Write a function that takes an array of values and moves all elements that are zero to the end of the array, otherwise preserving the order of the array. The zero elements must also maintain the order in which they occurred.
+
+For example, the following array
+
+[7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]
+
+is transformed into
+
+[7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0]
+
+Zero elements are defined by either 0 or "0". Some tests may include elements that are not number literals.
+
+You are NOT allowed to use any temporary arrays or objects. You are also not allowed to use any Array.prototype or Object.prototype methods.
+
+Sort "array" so that all elements with the value of zero are moved to the
+end of the array, while the other elements maintain order.
+[0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0]
+Zero elements also maintain order in which they occurred.
+[0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]
+  
+Do not use any temporary ARRAYS or OBJECTS. Additionally, you're not able
+to use any Array or Object prototype methods such as .shift(), .push(), etc
+
+Temporary variables are fine if necessary.
+  
+the correctly sorted array should be returned.
+
+*/
+
+const removeZeros = (array) => {
+	let limit = array.length;
+	let tmp;
+	for (let i = 0; i < limit; i++) {
+		if (array[i] === 0 || array[i] === "0") {
+			tmp = array[i];
+			// iterate again over the array
+			for (let j = i--; j < array.length - 1; j++) {
+				array[j] = array[j + 1];
+			}
+			// replace last value with the zero
+			array[array.length - 1] = tmp;
+			limit--;
+		}
+	}
+	return array;
+};
+
+// Steps:
+// 1. iterate over the array
+// 2. if the element is 0 or "0"
+// 3. save the element to a temporary variable
+// 4. iterate again over the array
+// 5. replace the element with the next element
+// 6. replace the last element with the temporary variable
+// 7. decrement the limit
+// 8. return the array
